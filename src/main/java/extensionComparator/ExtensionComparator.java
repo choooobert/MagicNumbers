cc256f4fe32d85879f66509de7b5e7da04ec1e9c
choooobert <48945021+choooobert@users.noreply.github.com>
2020-03-06 17:50:58 +0100

    another refactor - main class name changed to ExtensionComparator in order to match the convention

--- src/main/java/extensionComparator/ExtensionComparator.java
+++ src/main/java/extensionComparator/ExtensionComparator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class ExtensionComparator {
 
     public static void main(String[] args) {
 		try {
